a86808670417ce055c130c1f003b2e2bd4435ee0
Abhinavc2p2 <abhi77385@gmail.com>
2023-04-25 22:26:44 +0530

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return ( <
+        div >
+        <
+        h3 > kharcho - pe - nazar < /h3>  <
+        /div>
+    )
+}
+
+export default Header
---
+++ src/component/Transaction.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+export const Transaction = ({ transactions }) => {
+    const { deleteTransactions } = useContext(GlobalContext)
+    const sign = transactions.amount < 0 ? '-' : '+';
+    return ( <
+        li className = { transactions.amount < 0 ? 'minus' : 'plus' } > { transactions.text } < span > { sign } { Math.abs(transactions.amount) }₹ < /span> <
+        button className = "delete-btn"
+        onClick = {
+            () => deleteTransactions(transactions.id) } > x < /button> <
+        /li>
+    )
+}
+
+// export default Transaction
